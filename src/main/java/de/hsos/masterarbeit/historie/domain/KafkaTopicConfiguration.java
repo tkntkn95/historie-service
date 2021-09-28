4f360e7e21ac36a480c172b21fe7e790a3668121
KUMBARJI Omar <omar.kumbarji@soprasteria.com>
2021-09-28 16:11:30 +0200

    confing letzer versuch2

--- src/main/java/de/hsos/masterarbeit/historie/domain/KafkaTopicConfiguration.java
+++ src/main/java/de/hsos/masterarbeit/historie/domain/KafkaTopicConfiguration.java
@@ -21,7 +21,7 @@ public class KafkaTopicConfiguration {
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "articleservice.servicebus.windows.net:9092");
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "articleservice.servicebus.windows.net:9093");
         configs.put("sasl.mechanism", "PLAIN");
         configs.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule   required username='$ConnectionString'   password='Endpoint=sb://articleservice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rQgOzPQExZtmeAHqx+62v5qZmZfuz5+UfEiwAc1DURI=';");
         configs.put("security.protocol", "SASL_SSL");
